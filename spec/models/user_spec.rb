1dbb847953a1725aa2e03f674703eb4abfb5332e
Costa Nonato <costa@nonato.com>
2018-03-27 03:06:43 +0000

    Improving rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,6 @@ RSpec.describe User, type: :model do
   it { expect(@user).to respond_to(:password) }
   it { expect(@user).to respond_to(:password_confirmation) }
   it { expect(@user).to be_valid }
+
+  
 end
